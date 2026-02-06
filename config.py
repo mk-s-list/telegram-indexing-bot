9858fa560cc9c606764c870304ce2b37d689515a
mk-s-list <andsomeone58@gmail.com>
2026-02-06 17:01:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+MONGO_URI = "your_mongodb_url"
+DB_NAME = "IndexBot"
+
+FILE_CHANNEL = -100xxxxxxxxx
+ADMIN_ID = 123456789
